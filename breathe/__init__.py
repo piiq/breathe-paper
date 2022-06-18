fd730f477b8243f1b3e9475ad6c8a4f49e16ba5e
Theodore Aptekarev <aptekarev@gmail.com>
2022-06-18 12:28:52 +0200

    Update conda environment

---
+++ breathe/__init__.py
@@ -0,0 +1,6 @@
+"""BGU Respiratory Pathologies Discovery Toolkit.
+
+This module contains helper functions that facilitate research and discovery in jupyter
+notebooks. The helpers are based on different python libraries and can be used both
+separately and in connection with each other.
+"""
